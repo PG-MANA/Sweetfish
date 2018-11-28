8fe5283da7ad77d17c50a378eacb3e71db1478aa
XeonSquared <redtailedairways@gmail.com>
2018-11-29 03:06:32 +1100

    added some code to try to load a translation for the current locale

--- src/Sweetfish.cpp
+++ src/Sweetfish.cpp
@@ -4,11 +4,18 @@
  */
 #include "Sweetfish.h"
 #include "UI/MainWindow.h"
+#include <QTranslator>
 #include <QApplication>
 #include <QTextCodec>
 
 int main(int argc, char *argv[]) {
   QApplication app(argc, argv);
+  // hacky translator stuff I picked up off the qt tutorials https://doc.qt.io/qt-5/qtlinguist-arrowpad-example.html
+  // attempts to load a translation file for the current locale
+  QTranslator translator;
+  QString locale = QLocale::system().name();
+  translator.load(QString("sweetfish-") + locale);
+  app.installTranslator(&translator);
   //全般設定
   QTextCodec::setCodecForLocale(QTextCodec::codecForName("UTF-8"));
   app.setWindowIcon(QIcon(":/icon-normal.png")); //埋め込み
